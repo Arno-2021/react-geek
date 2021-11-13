c431705ca2104a6f6a9d29e489ff4eae5e184c6a
Arno-2021 <aa307204890@gmail.com>
2021-11-13 20:26:58 +0800

    layout---inject module scss demo

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Button } from 'antd'
+import styles from './index.module.scss'
 export default function Layout() {
     return (
-        <div>
+        <div className={styles.demo}>
             <Button type='primary'>BUTTON!</Button>
         </div>
     )
